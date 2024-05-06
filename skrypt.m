3209185bafe393d86c4a2a4364161d5da345d383
trzmjel <orgimi@pm.me>
2024-05-06 18:25:37 +0200

    Pierwsza wersja skryptu
    Posiada ona najbardziej podstawowe założenia, na jego bazie
    najprawdobodobniej zostanie utworzone więcej skryptów, umożliwiające
    uzyskanie rezultatu dla każdego etapu.

---
+++ skrypt.m
@@ -0,0 +1,28 @@
+% Parametry sygnału
+amplituda = 1;
+czestotliwosc = 1; % Hz
+czas_trwania = 2; % sekundy
+czestotliwosc_probkowania = 1000; % Hz
+
+% Czas sygnału
+t = linspace(0, czas_trwania, czas_trwania * czestotliwosc_probkowania);
+
+% Generowanie sygnału sinusoidalnego
+sygnal = amplituda * sin(2 * pi * czestotliwosc * t);
+
+% Transformaty Fouriera
+widmo = abs(fft(sygnal));
+f = linspace(0, czestotliwosc_probkowania, length(widmo));
+
+% Wykresy
+subplot(2, 1, 1);
+plot(t, sygnal);
+title('Sygnał sinusoidalny w dziedzinie czasu');
+xlabel('Czas [s]');
+ylabel('Amplituda');
+
+subplot(2, 1, 2);
+plot(f, widmo);
+title('Widmo częstotliwościowe');
+xlabel('Częstotliwość [Hz]');
+ylabel('Amplituda');
